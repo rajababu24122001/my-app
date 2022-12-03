e25613eace437f488d3bd760398b206550547714
Raja <rb5934356@gmail.com>
2022-12-03 13:01:31 +0530

    my first commit my-app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        This is Raja , 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Learning Mern-stack Development..
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
